1146372bcfd3d924c2cc006a4d71b13201a62512
Moto <jorge.moto@hyperscayle.com>
2026-06-21 20:10:41 -0600

    Add middleware and view engine configuration

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const axios   = require('axios');
 const app     = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.set('view engine', 'pug');
+app.set('views', './views');
